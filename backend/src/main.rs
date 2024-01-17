5729ece73c51d423b898f0cb15f79a6488c030bd
cal <callum.adair@outlook.com>
2024-01-17 22:22:36 +0000

    fixed typos and switched to using scopes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,10 +7,34 @@ async fn main() -> std::io::Result<()> {
     let subscriber = tracing_subscriber::fmt::init();
 
     HttpServer::new(|| {
+        let task_scope = web::scope("/tasks")
+            .service(task_index)
+            .service(task_store)
+            .service(task_show)
+            .service(task_destroy)
+            .service(task_update);
+        let category_scope = web::scope("/finance/categories")
+            .service(category_index)
+            .service(category_store)
+            .service(category_show)
+            .service(category_destroy)
+            .service(category_update);
+        let habit_scope = web::scope("/habits")
+            .service(habit_index)
+            .service(habit_store)
+            .service(habit_show)
+            .service(habit_destroy)
+            .service(habit_update);
+        let settings_scope = web::scope("/settings")
+            .service(settings_index)
+            .service(settings_update);
+
         App::new()
             .wrap(Logger::default())
-            .service(index)
-            .service(settings_index)
+            .service(task_scope)
+            .service(category_scope)
+            .service(habit_scope)
+            .service(settings_scope)
     })
     .bind(("0.0.0.0", 8080))?
     .run()
@@ -22,87 +46,87 @@ async fn index() -> impl Responder {
     HttpResponse::Ok().body("This is a test str")
 }
 
-#[get("/tasks")]
+#[get("/")]
 async fn task_index() -> impl Responder {
     HttpResponse::Ok().body("This is a test str")
 }
 
-#[post("/tasks")]
+#[post("/")]
 async fn task_store() -> impl Responder {
     HttpResponse::Ok().body("This is a test str")
 }
 
-#[get("/tasks/{id}")]
+#[get("/{id}")]
 async fn task_show(path: web::Path<(u32)>) -> impl Responder {
     HttpResponse::Ok().body("This is a test str")
 }
 
-#[delete("/tasks/{id}")]
+#[delete("/{id}")]
 async fn task_destroy(path: web::Path<(u32)>) -> impl Responder {
     HttpResponse::Ok().body("{id}")
 }
 
-#[patch("/tasks/{id}")]
+#[patch("/{id}")]
 async fn task_update(path: web::Path<(u32)>) -> impl Responder {
     HttpResponse::Ok().body("{id}")
 }
 
-#[get("/finance/categories")]
+#[get("/")]
 async fn category_index() -> impl Responder {
     HttpResponse::Ok().body("This is a test str")
 }
 
-#[post("/finance/categories")]
+#[post("/")]
 async fn category_store() -> impl Responder {
     HttpResponse::Ok().body("This is a test str")
 }
 
-#[get("/finance/categories/{id}")]
+#[get("/{id}")]
 async fn category_show(path: web::Path<(u32)>) -> impl Responder {
     HttpResponse::Ok().body("This is a test str")
 }
 
-#[delete("/finance/categories/{id}")]
-async fn category_delete(path: web::Path<(u32)>) -> impl Responder {
+#[delete("/{id}")]
+async fn category_destroy(path: web::Path<(u32)>) -> impl Responder {
     HttpResponse::Ok().body("This is a test str")
 }
 
-#[patch("/finance/categories/{id}")]
+#[patch("/{id}")]
 async fn category_update(path: web::Path<(u32)>) -> impl Responder {
     HttpResponse::Ok().body("This is a test str")
 }
 
-#[get("/habits")]
+#[get("/")]
 async fn habit_index() -> impl Responder {
     HttpResponse::Ok().body("This is a test str")
 }
 
-#[post("/habits")]
+#[post("/")]
 async fn habit_store() -> impl Responder {
     HttpResponse::Ok().body("This is a test str")
 }
 
-#[get("/habits/{id}")]
+#[get("/{id}")]
 async fn habit_show(path: web::Path<(u32)>) -> impl Responder {
     HttpResponse::Ok().body("This is a test str")
 }
 
-#[delete("/habits/{id}")]
-async fn habit_destory(path: web::Path<(u32)>) -> impl Responder {
+#[delete("/{id}")]
+async fn habit_destroy(path: web::Path<(u32)>) -> impl Responder {
     HttpResponse::Ok().body("This is a test str")
 }
 
-#[patch("/habits/{id}")]
+#[patch("/{id}")]
 async fn habit_update(path: web::Path<(u32t)>) -> impl Responder {
     HttpResponse::Ok().body("This is a test str")
 }
 
-#[get("/settings")]
+#[get("/")]
 async fn settings_index() -> impl Responder {
     HttpResponse::Ok().body("This is a test str")
 }
 
-#[patch("/settings")]
+#[patch("/")]
 async fn settings_update() -> impl Responder {
     HttpResponse::Ok().body("This is a test str")
 }
